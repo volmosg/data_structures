6b653c491cc15933fe72a15acb8f8bab4bab09a3
volmo <volmosg.bt@gmail.com>
2020-03-16 18:50:21 +0100

    started to implement a circular singly linked list. Need to come back to theory

---
+++ dataStructures/src/main/java/vog/java/circularList/CircularList.java
@@ -0,0 +1,40 @@
+package vog.java.circularList;
+
+public class CircularList<E> {
+	// singly circular list
+	private int size;
+	
+	public CircularList(){
+		size = 0;
+	}
+	
+	//TODO add
+	public boolean add(E data) {
+		if (data == null) {
+			throw new NullPointerException();
+		}
+		if (this.size == 0) {
+			CircularNode<E> newNode = new CircularNode<E>(data);
+			size +=1;
+			return true;
+		}
+		else {
+			CircularNode<E> newNode = new CircularNode<E>(data);
+			//TODO need to add a last pointer
+		}
+		
+		return false;
+		
+	}
+	
+	//TODO remove
+	
+	//TODO get
+	
+	//TODO set
+	
+	//TODO add E int
+	
+	//TODO size
+
+}
---
+++ dataStructures/src/main/java/vog/java/circularList/CircularNode.java
@@ -0,0 +1,15 @@
+package vog.java.circularList;
+
+public class CircularNode<E> {
+	E data;
+	// singly
+	CircularNode<E> nextNode;
+	
+	public CircularNode(E e){
+		this.data = e;
+		// the most basic circular list will be a single element circular list
+		// pointing to itself
+		this.nextNode = this;
+	}
+
+}
